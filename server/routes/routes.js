e21b33a9460f6f439d633744779f5a6018f54d88
CulesBao <baocules17@gmail.com>
2024-08-09 10:19:58 +0700

    add get method

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -4,5 +4,8 @@ import express from 'express'
 const routes = express.Router()
 
 routes.use('/auth', authRoutes)
+routes.get('/', (req, res) => {
+    console.log("This is chat-app presented by Cules")
+})
 
 export default routes
