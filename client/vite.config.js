584c0b853f4efaeddb64e5e3975693349edd5974
CulesBao <baocules17@gmail.com>
2024-08-17 09:21:20 +0700

    add proxy in vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,11 +5,19 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // Điều này cho phép truy cập từ địa chỉ IP hoặc tên miền khác ngoài localhost
+    host: true, // Cho phép truy cập từ địa chỉ IP hoặc tên miền khác ngoài localhost
+    port: 5173, // Cổng mà Vite sẽ lắng nghe
     hmr: {
       host: 'baocules.me', // Địa chỉ tên miền của bạn
       protocol: 'wss', // Sử dụng wss cho WebSocket qua HTTPS
       port: 5173, // Đảm bảo rằng cổng này đang được mở trên server EC2
-    }
-  }
+    },
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000', // Địa chỉ của server API
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
