cb11a4ccaa09c01745b65b2f2a1ba71719692c21
CulesBao <baocules17@gmail.com>
2024-08-24 17:40:12 +0700

    Hidden URI in client:

--- client/src/utils/service.js
+++ client/src/utils/service.js
@@ -1,4 +1,5 @@
-const baseUrl = 'https://baocules.me/api'
+const baseUrl = import.meta.env.VITE_LOCAL_API_URI;
+
 const postRequest = async(url, body) =>{
     const response = await fetch(url, {
         method: "POST",
@@ -9,27 +10,10 @@ const postRequest = async(url, body) =>{
     })
 
     const data = await response.json()
-    // console.log(response)
-    // console.log(data)
     return { 
         status: response.status, 
         ...data
     };
-
-    // if (!response.oke){
-    //     let message;
-    //     if (data?.message)
-    //         message = data.message
-    //     else
-    //         message = data
-
-    //     return {
-    //         error: true,
-    //         message
-    //     }
-    // }
-
-    return data
 }
 
 export default {baseUrl, postRequest}
