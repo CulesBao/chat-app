c5c8ebc0fc8ba57cb84bcf1b6da5f6553a43d2d5
CulesBao <baocules17@gmail.com>
2024-09-01 16:20:24 +0700

    feature/chat-apis: Create collection chats

---
+++ server/models/chats.models.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const chatsSchema = new mongoose.Schema(
+    {
+        members: Array
+    },
+    {
+        timestamps: true
+    }
+)
+
+const chats = mongoose.model('chats', chatsSchema)
+export default chats
